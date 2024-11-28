4492856f326eef143f8a1472acb8bf2572c9dff3
Orosxco <ronaldorosoco9@gmail.com>
2024-11-28 20:24:13 +0500

    submit to codingal

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hello World")
+print(23)
+print("How are you?","Im fine.")
+import keyword
+print(keyword.kwlist)
---
+++ operators and data types.py
@@ -0,0 +1,13 @@
+name = "Penguin"
+age=15
+Employed = False
+weight = 43.7
+
+print("The data type of name is ", type(name))
+print("The data type of age is ", type(age))
+print("The data type of Employed is ", type(Employed))
+print("the data type of weight is ", type(weight))
+
+print("\n After type casting... ")
+age = str(age)
+print("The data type of age is now ", type(age))
